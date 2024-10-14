ad7ea20178e3b486beab6fe11a952e9f8e91e620
sithijakavee <94134102+sithijakavee@users.noreply.github.com>
2024-10-14 01:01:07 -0700

    Update Loader

--- src/components/BookLoader.jsx
+++ src/components/BookLoader.jsx
@@ -3,8 +3,10 @@ import React from "react";
 
 const BookLoader = () => {
   return (
-    <div className="overflow-hidden fixed top-0  z-100 h-screen w-screen ">
-      <h1 className="text-black absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] m-auto  ">Loading.....</h1>
+    <div className="overflow-hidden fixed top-0  z-100 h-screen w-screen bg-black">
+      
+      <img src="/images/loading.gif" alt="loading" srcset="" className="w-[50%] h-fit rounded-full absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] m-auto"/>
+      <h1 className="text-white absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] m-auto ">Last line, Gimme a sec.....</h1>
     </div>
   );
 };
